564db1aa987244b7f18479f8c0d00a896ed2c7b1
Tony Xie <tonis.xie@gmail.com>
2018-08-11 11:00:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import urllib
-f = urllib.urlopen("http://ww1.yorkmaps.ca/arcgis/rest/services/Hosted?f=pjson")
-print f.read()
+import json
+params = urllib.urlencode({'f': 'pjson'})
+f = urllib.urlopen("http://ww1.yorkmaps.ca/arcgis/rest/services/Hosted?%s" % params)
+result = f.read()
+data = json.loads(result)
+print data
