8b93c2b9ef84cadaaca842cd842be370362457cd
Tony Xie <tonis.xie@gmail.com>
2018-08-11 11:59:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import urllib
 import json
+from datetime import datetime
+
+def output(line):
+    line = '{0} {1}'.format(datetime.now().isoformat(), line)
+    f = open('log.txt', 'a+')
+    f.write(line+'\n')
+    f.close()
+    print line
 
 def getData(uri, where=''):
     params = urllib.urlencode({'f': 'pjson'}) if where == '' else urllib.urlencode({'f': 'pjson', 'where': where})
     baseurl = 'http://ww1.yorkmaps.ca/arcgis/rest/services/'
     fullurl = '{0}{1}?{2}'.format(baseurl, uri, params)
-    print fullurl
+    output(fullurl)
     f = urllib.urlopen(fullurl)
     result = f.read()
     return json.loads(result)
@@ -20,6 +28,6 @@ for service in services:
         uri += '/{0}/query'.format(layer['id'])
         data = getData(uri, 'ObjectId>0')
         if 'features' in data:
-            print len(data['features'])
+            output(len(data['features']))
         else:
-            print data  
+            output(data)
